e09ca12ee97356b0dda28bff30991b4ff71c42b9
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2022-04-14 19:17:19 +0100

    Fixed links

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -11,7 +11,9 @@ const Project = () => {
       <div id="project-section">
         <div className="box">
           <div className="imgBx">
-            <img src={Project1} alt="" />
+            <a href="https://mdrc.vercel.app/" target="_blank">
+              <img src={Project1} alt="" />
+            </a>
           </div>
           <div className="content">
             <h3>Milford Resource Centre</h3>
@@ -31,7 +33,9 @@ const Project = () => {
 
         <div className="box">
           <div className="imgBx">
-            <img src={Project2} alt="" />
+            <a href="https://clubstub-fe.vercel.app/home" target="_blank">
+              <img src={Project2} alt="" />
+            </a>
           </div>
           <div className="content">
             <h3>O'Deals GAA Tickets</h3>
@@ -52,7 +56,9 @@ const Project = () => {
 
         <div className="box">
           <div className="imgBx">
-            <img src={Project3} alt="" />
+            <a href="https://darts-calculator.vercel.app" target="_blank">
+              <img src={Project3} alt="" />
+            </a>
           </div>
           <div className="content">
             <h3>Darts Score Browser App</h3>
@@ -73,17 +79,19 @@ const Project = () => {
 
         <div className="box">
           <div className="imgBx">
-            <img src={Project4} alt="" />
+            <a href="https://end-of-day-ts.vercel.app//" target="_blank">
+              <img src={Project4} alt="" />
+            </a>
           </div>
           <div className="content">
             <h3>End of Day Browser App</h3>
 
             <div className="content-links">
-              <a href="https://end-of-day-counter.vercel.app/" target="_blank">
+              <a href="https://end-of-day-ts.vercel.app//" target="_blank">
                 <span>Live site</span>
               </a>
               <a
-                href="https://github.com/barrytrearty/EndOfDay-Counter"
+                href="https://github.com/barrytrearty/EndOfDay-TS"
                 target="_blank"
               >
                 <span>Source code</span>
